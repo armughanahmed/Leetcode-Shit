7407206ec63eaf1ab4c05f4dd1e9773a53f9ce8a
armughanahmed <50084543+armughanahmed@users.noreply.github.com>
2025-10-03 16:05:49 +0200

    Time: 44 ms (25.15%), Space: 72.9 MB (38.55%) - LeetHub

---
+++ 0015-3sum/0015-3sum-twopointer.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+    const ans = [];
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums.length; i++) {
+        while (i < nums.length && nums[i] == nums[i - 1]) {
+            i++;
+        }
+
+        let j = i + 1;
+        let k = nums.length - 1;
+        while (j < k) {
+            const sum = nums[i] + nums[j] + nums[k];
+            if (sum == 0) {
+                ans.push([nums[i], nums[j], nums[k]]);
+                j++;
+                k--;
+                while (j < k && nums[j] == nums[j - 1]) {
+                    j++;
+                }
+
+                while (j < k && nums[k] == nums[k + 1]) {
+                    k--;
+                }
+            }
+            else if (sum > 0) {
+                k--;
+            }
+            else {
+                j++;
+            }
+        }
+    }
+
+    return ans;
+};
