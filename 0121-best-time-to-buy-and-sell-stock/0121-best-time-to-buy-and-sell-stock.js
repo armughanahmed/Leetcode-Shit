a3e1f63ac653fd4d7cbc9c861c2f622e5e6d7766
armughanahmed <50084543+armughanahmed@users.noreply.github.com>
2025-10-04 19:12:57 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let max=0;
+    let l=0;
+    let r=l+1;
+    while(r<prices.length){
+        max = Math.max(max, prices[r]-prices[l]);
+        if(prices[l]<prices[r]){
+            r++;
+        }
+        else{
+            l++;
+            r++;
+        }
+    }
+
+    return max;
+};
